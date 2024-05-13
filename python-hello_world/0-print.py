98e31e3f5b3514d0986d443089aa6142d641d612
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-13 04:15:48 -0500

    add readme and images to main folder, print a given string with  out the use of variable

---
+++ python-hello_world/0-print.py
@@ -0,0 +1,4 @@
+# Print a given string without the use of a variable
+# String to print "Programming is like building a multilingual puzzle
+
+print('"Programming is like building a multilingual puzzle')
