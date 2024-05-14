e0267a1d62f193632cd41e4e58039e08537c0ea8
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-14 01:57:21 -0500

    update all codes

---
+++ python-hello_world/6-concat_edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Complete this source code to print object-oriented
+ programming with Python, followed by a new line."""
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+
+str = str[39:66] +" "+ str[110:114] +" "+ str[:6]
+print(str)
---
+++ python-hello_world/7-easter_egg.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""This code prints the Zen of Python"""
+import this
+
+print(this)
