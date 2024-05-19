33953c5cf126b0bda614b43257c9f4252ab1d086
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-19 04:05:21 -0500

    print numbers from 0 through 99 in two digits comma separated

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+"""
+This program will print decimal numbers from 0 through
+99 separated by comma. All numbers will be printed as two
+digits number.
+"""
+
+for i in range(100):
+  if i != 99:
+    print(f'{i:02},', end=' ')
+  else:
+    print(i)
