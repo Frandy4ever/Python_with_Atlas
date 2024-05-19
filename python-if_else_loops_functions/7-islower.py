bbc79a2b209f462e69f8074cf83179a964cdd2da
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-19 05:36:46 -0500

    eval if an given Ascii alphabet character is in uppercase or lowercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+This program will eval if aa given Ascii alphabet
+character is in lowercase of uppercase.
+"""
+
+def isLower(c):
+    return ord('a') <= ord(c) <= ord('z')
