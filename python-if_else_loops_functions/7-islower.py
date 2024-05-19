c110fd3aede71aabe8d3791be8a4b8f615d76a7f
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-19 05:56:50 -0500

    update typing and add main guard

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -5,5 +5,8 @@ This program will eval if aa given Ascii alphabet
 character is in lowercase of uppercase.
 """
 
-def isLower(c):
+def isLower(c: chr) -> bool:
     return ord('a') <= ord(c) <= ord('z')
+
+if __name__ == "__main__":
+    print(isLower('p'))
