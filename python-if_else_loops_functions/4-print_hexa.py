55496b8274752f62e1b0dbd8a59916a2af688014
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-19 03:18:05 -0500

    print decimal and hex numbers from 0 to 98

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+This program print decimal numbers from 0 through 98
+alongside their hexadecimal equivalent.
+"""
+
+for n in range(99):
+    print("{} = 0x{:x}".format(n, n))
