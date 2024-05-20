e09a31a615a6811d2ec8a45f7520c29beee4ee27
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-20 00:39:28 -0500

    convert all lowercase characters to uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+This program will convert every Ascii characters
+between 'a' and 'z' inclusive, found within 
+`str` param to uppercase.
+"""
+def isUpper(str: str) -> None:
+    for i in str:
+        i = ord(i)
+        if ord('a') <= i <= ord('z'):
+            i -= 32
+        print(chr(i), end='')
+    print()
