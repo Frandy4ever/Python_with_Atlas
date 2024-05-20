673340d7c5d8e24e848d6d14b62e34a3c92cb6f5
Frandy Slueue <frandy4ever@gmail.com>
2024-05-20 17:09:10 -0500

    add typing

--- python-hello_world/5-edges.py
+++ python-hello_world/5-edges.py
@@ -5,9 +5,9 @@ This program prints the first 3 letters of a given string variable,
 then it prints the last two letters followed by printing all letters
 but the first and last letters of the same string, using string format.
 """
-word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
+word: str = "Holberton"
+word_first_3: str = word[:3]
+word_last_2: str = word[-2:]
+middle_word: str = word[1:-1]
 
 print("{}\n{}\n{}".format(word_first_3, word_last_2, middle_word))
--- python-hello_world/6-concat_edges.py
+++ python-hello_world/6-concat_edges.py
@@ -4,8 +4,8 @@
 This program will take particular parts of a given string
 to form a new string and print its value, using string format.
 """
-str = "Python is an interpreted, interactive, object-oriented programming\
+string: str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
 
-new_str = str[39:66] +" "+ str[110:114] +" "+ str[:6]
+new_str: str = string[39:66] +" "+ string[110:114] +" "+ string[:6]
 print("{}".format(new_str))
