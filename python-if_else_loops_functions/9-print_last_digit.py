873a4abe27388f28d5834cb10ed32a115bea4dd5
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-20 01:28:17 -0500

    print the last digit of a given integer

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+This program will print and or return the last digit
+of a signed integer given to the param.
+"""
+def print_last_digit(number: int) -> int:
+    """Prints the last digid of `number` value."""
+    print(number % 10, end='')
+    
+    """Returns the last digit of `number` value."""
+    return (number % 10)
