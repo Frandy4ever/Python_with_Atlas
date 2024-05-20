c663ad604bc451aa25aced3d34eb323b9690f814
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-20 03:48:46 -0500

    remove element at `n` index

--- python-if_else_loops_functions/13-print_tebahpla.py
+++ python-if_else_loops_functions/13-print_tebahpla.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+"""
+This program will remove character at `n` index
+of a copy of a given string.
+"""
+def remove_char_at(str: str, n: int) -> None:
+    """Return new string without the value
+    at n index"""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        print(str[i], end='')
+    print()
