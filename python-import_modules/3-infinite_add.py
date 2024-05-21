94a3f0dc44e4973d6728307f5dd86d8913c670d1
Frandy Slueue <frandy4ever@gmail.com>
2024-05-21 07:05:03 -0500

    Computes the sum of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import sys
+from typing import List
+
+"""
+This program will print sum of all args.
+"""
+
+def add_args() -> None:
+    """Computes the sum of all arguments"""
+    args: List[str] = sys.argv[1:]
+    result: int = 0
+    for i in args:
+        result += int(i)
+    
+    print(result)
+
+if __name__ == ('__main__'):
+    add_args()
