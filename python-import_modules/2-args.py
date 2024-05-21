b6540ad13b211ea7011ac3664fc88356a4cc1402
Frandy Slueue <frandy4ever@gmail.com>
2024-05-21 06:51:55 -0500

    Keeps track and count the number of args passed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+This program will print the count of args pass along with
+the arg number and arg name.
+"""
+import sys
+
+
+def main():
+    '''Keeps track and count the number of args passed.'''
+    args = sys.argv[1:]
+    count = len(sys.argv)
+
+    if count == 0:
+        print('0 argument.')
+    elif count == 1:
+        print('1 argument:')
+    else:
+        print(f'{count} arguments:')
+    
+    for i, arg in enumerate(args, start=1):
+        print(f'{i}: {arg}')
+
+if __name__ == ("__main__"):
+    main()
