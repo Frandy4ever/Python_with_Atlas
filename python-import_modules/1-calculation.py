ee927e6c7308137c0c86f862173a3a5e6edbe1f4
Frandy Slueue <frandy4ever@gmail.com>
2024-05-20 21:08:33 -0500

    use the fn of an imported module

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+This program imports calculator_1 module as cal
+and makes use of its functionalities.
+"""
+if __name__ == ('__main__'):
+    import calculator_1 as cal
+
+    a: int = 10
+    b: int = 5
+
+    print(f"{a} + {b} = {cal.add(a, b)}")
+    print(f"{a} - {b} = {cal.sub(a, b)}")
+    print(f"{a} * {b} = {cal.mul(a, b)}")
+    print(f"{a} / {b} = {cal.div(a, b)}")
