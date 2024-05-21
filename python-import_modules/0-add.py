e0feea76d0cd0e733d782d34fa01884a7edff232
Frandy Slueue <frandy4ever@gmail.com>
2024-05-20 20:47:20 -0500

    use a fn from and imported module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This program imports add_0 module and uses its add() fn to print the sum of `a` and `b`.
+"""
+if __name__ == ("__main__"):
+    from add_0 import add
+    """Print the sum of a and b"""
+    a: int = 1
+    b: int = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
