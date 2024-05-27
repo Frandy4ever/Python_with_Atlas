211cbcb8489760a2aa830c4faca4a4a73dab561a
Frandy Slueue <frandy4ever@gmail.com>
2024-05-27 07:46:48 -0500

    print all integer of a list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+
+"""This program prints all integer elements of a list"""
+from typing import List
+def print_list_integer(my_list: List[int] = []) -> None:
+    for n in my_list:
+        print("{}".format(n))
