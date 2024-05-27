4e475f630ca77d027dd621fb0d78072997002573
Frandy Slueue <frandy4ever@gmail.com>
2024-05-27 07:56:15 -0500

    retrieve element at a given index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""This program retrieves an element from a list"""
+from typing import Union
+def element_at(my_list: Union[int, str], idx: int) -> Union[None, int]:
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
