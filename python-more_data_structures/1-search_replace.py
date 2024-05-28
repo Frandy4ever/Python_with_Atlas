9f2809aaa4494217ed43715b3ceaa585f091227d
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 05:56:38 -0500

    create a new list where an all occurances of an element if replaced with a given element

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""This program replaces all occurrences of
+an element by another in a new list."""
+
+from typing import List
+
+def search_replace(my_list: List[int], search: int, replace: int) -> List[int]:
+    """Returns a new list where search is replaced by replace"""
+    return [replace if i == search else i for i in my_list]
