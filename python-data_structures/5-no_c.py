0fba4d21cfbe62477ada3578f3fd874c7ee548d7
Frandy Slueue <frandy4ever@gmail.com>
2024-05-27 23:07:10 -0500

    remove all c and C from string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""This program removes all characters c and C from a string."""
+
+def no_c(my_string: str) -> str:
+    """Remove all 'c' and 'C' from a string."""
+    new_str = ''.join([char for char in my_string
+                       if char != 'c' and char != 'C'])
+    return new_str
