7db762ee0bbc91ca63d47646088d0c2f814e803b
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 10:22:21 -0500

    get the number of a dictionary keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+"""This program returns the number of keys in a dictionary"""
+
+from typing import Dict
+
+# return (len(a_dictionary.keys()))
+def number_keys(a_dictionary: Dict[int, str]) -> int:
+    """Returns the number of a_dictionary keys"""
+    return len(a_dictionary)
