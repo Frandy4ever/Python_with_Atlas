a8e3d19b736d4bc47f67a3d4e5f82b07c1746beb
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 05:39:21 -0500

    calc the square value of each element in the matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""This program computes the square value of all integers of a matrix. """
+from typing import List
+
+
+def square_matrix_simple(matrix: List[List[int]] = []) -> List[List[int]]:
+    """Returns the square value of each element in the matrix"""
+    return [[elm * elm for elm in row] for row in matrix]
