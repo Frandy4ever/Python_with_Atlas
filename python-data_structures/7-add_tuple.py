a57e0fc6dfaaf0bbaa670dcb78f748d5ecd6154f
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 01:49:11 -0500

    print the sum of two tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This program adds 2 tuples"""
+from typing import Tuple
+
+
+def add_tuple(tuple_a: Tuple[int, ...]=(),
+              tuple_b: Tuple[int, ...] = ()) -> Tuple[int, int]:
+    """"Returns the sum of two tuples"""
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+    return (a1 + b1, a2 + b2)
