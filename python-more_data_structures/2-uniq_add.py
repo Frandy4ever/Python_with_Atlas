2d4cd3f1b36ddc9df983d73520f3a6fa4aef20a2
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 09:33:16 -0500

    calc the sum of unique integers in a list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""This program  adds all unique integers in a list"""
+from typing import List
+
+def uniq_add(my_list: List[int]) -> int:
+    """Returns the sum of unique integers in my_list"""
+    return sum(set(my_list))
