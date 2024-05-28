80161b7f03d011611730b4f787f2f8df8e3b8cbb
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 01:58:49 -0500

    get the lenght and print the last character of a sentence

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""This program returns a tuple with the
+length of a string and its first character."""
+from typing import Tuple
+
+
+def multiple_returns(sentence: str) -> Tuple[int, chr]:
+    """Returns the length value and last char of sentence."""
+    return (len(sentence), sentence[0])
