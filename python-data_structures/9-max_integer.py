6d1dd54875dc879d7fb2ffcf343593ece5afc107
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 02:14:18 -0500

    print the greatest number of a list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+"""This program finds the biggest integer of a list."""
+from typing import List
+
+
+def max_integer(my_list: List[int] = []) -> int:
+    """Return the greatest number of my_list."""
+    if len(my_list) <= 0:
+        return None
+    
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    return max_num
