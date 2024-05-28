3aff0afe38107afbb97b3fbe86e97ab1378e6570
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 10:09:20 -0500

    get unique elements between two sets

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""This program returns a set of all elements present in only one set."""
+from typing import Set
+
+def only_diff_elements(set_1: Set[str], set_2: Set[str]) -> Set[str]:
+    """Returns unique elements between two sets."""
+    return set_1 | set_2
