2f65260ff18b61925606639b9a812021ad29ba5c
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 02:43:39 -0500

    delete element from a list with out the use of pop()

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+"""This program deletes the item at a specific position in a list."""
+from typing import List
+
+
+def delete_at(my_list: List[int] = [], idx: int = 0) -> List[int]:
+    """Return a the my_list modified"""
+    if len(my_list) < idx or idx < 0:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
