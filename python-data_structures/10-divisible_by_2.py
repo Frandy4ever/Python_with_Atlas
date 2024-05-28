5ce3cd0e4a8d4d9d679b6fd6b0a8e8f44eeff407
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 02:28:12 -0500

    check if elements in list are divisible by 2 or not

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This program finds all multiples of 2 in a list."""
+from typing import List
+
+
+def divisible_by_2(my_list: List[int] = []) -> List[bool]:
+    new_list = [i % 2 == 0 for i in my_list]
+    return new_list
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
