efeb30583fd6bbcc26c44ec631bc91d347124aab
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 00:52:43 -0500

    print matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""This program prints a matrix of integers."""
+from typing import List
+def print_matrix_integer(matrix: List[List[int]]) -> int:
+    for row in matrix:
+        print("\n")
+        for elm in row:
+            print(elm, end=" ")
