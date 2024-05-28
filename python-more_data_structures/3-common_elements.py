e6eb2afb6f81a9acadf93dc3c1ae46c4bb67feef
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 09:49:04 -0500

    get what's common between two sets

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+"""This program returns a set of common elements in two sets."""
+
+from typing import Set, Union
+
+
+def common_elements(set_1: Set[Union[int, str]],
+                    set_2: Set[Union[int, str]]) -> Set[Union[int, str]]:
+    """Returns what's common between two sets."""
+    return [elm1 for elm1 in set_1 for elm2 in set_2 if elm1 == elm2]
