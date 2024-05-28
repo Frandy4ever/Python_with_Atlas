15976d6ea664dec1adcc4ef33ba03cc593092ceb
Frandy Slueue <frandy4ever@gmail.com>
2024-05-28 02:29:07 -0500

    add comments

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,6 +4,7 @@ from typing import List
 
 
 def divisible_by_2(my_list: List[int] = []) -> List[bool]:
+    """Check if list elements are divisible by 2 or not."""
     new_list = [i % 2 == 0 for i in my_list]
     return new_list
 
