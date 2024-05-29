e29bd17584c1fcb528870ff41cabf15d2f5cc7f5
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 02:27:21 -0500

    sort a dictionary by order of keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""This program prints a dictionary by ordered keys"""
+from typing import Dict
+
+
+def print_sorted_dictionary(a_dictionary: Dict[int, str]) -> Dict[int, str]:
+    """Returns a dictionary sorted by keys."""
+    return {key: a_dictionary[key] for key in sorted(a_dictionary)}
