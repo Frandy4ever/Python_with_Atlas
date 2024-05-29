abe46021cc2d4acc9bc263077902e074444f9c77
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 05:34:26 -0500

    multiply the each dict value

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+"""This program returns a new dictionary
+with all values multiplied by 2"""
+from typing import Dict
+
+
+def multiply_by_2(a_dictionary: Dict[str, int]) -> Dict[str, int]:
+    """Returns a copy of a_dictionary with each value multiplied by 2"""
+    return {key: val * 2 for key, val in a_dictionary.items()}
