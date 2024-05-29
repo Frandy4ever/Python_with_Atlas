b5caa2b7c76f02df373c470176326d9fffceecfc
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 13:58:54 -0500

    update key or value when possible

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+"""This program replaces or adds key/value in a dictionary."""
+from typing import Union, Dict
+
+
+def update_dictionary(a_dictionary: Dict[int, str],
+                      key: str, value: Union[int, str]) -> Dict[int, str]:
+    """Update key and or value when possible."""
+    a_dictionary[key] = value
+    return a_dictionary
