5459c661eb28ccfd0716c47345ab942a7b8b4fff
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 11:53:13 -0500

    get the biggest number

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+"""This program returns a key with the biggest integer value."""
+from typing import Dict
+
+
+def best_score(a_dictionary: Dict[str, int]) -> int:
+    """Returns the greatest score."""
+    max_score = 0
+    result = ""
+    if not a_dictionary:
+        return None
+    else:
+        for k, v in a_dictionary.items():
+            if v > max_score:
+                max_score = v
+                result = k
+    return result
