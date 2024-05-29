eb0c939d48f85673ff247883f256cb95355bf8a5
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 04:49:28 -0500

    delete a dictionary key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""This program deletes a key in a dictionary."""
+from typing import Dict
+
+
+def simple_delete(a_dictionary: Dict[int, str], key: str = ''):
+    """Returns a modified dictionary."""
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
