a771ad6e9c01305fab30715a971ebfa6e4711072
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-30 12:07:22 -0500

    delete element from a dict if the given value exists

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+"""This program deletes keys with a
+specific value in a dictionary."""
+
+from typing import Dict, Union
+
+
+def complex_delete(a_dictionary: Dict[int, str],
+                   value: Union[int, str]) -> Dict[int, str]:
+    """Delete a value from a_dictionary if it exist"""
+    temp_dict = a_dictionary.copy()
+    
+    for k, v in temp_dict.items():
+        if value == v:
+            del a_dictionary[k]
+    
+    return a_dictionary
