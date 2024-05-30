ee9edc377f18c67e9197e054f34a46f4ea703367
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-30 05:38:24 -0500

    calc tuple average

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+"""This program returns the weighted average of all integers tuple."""
+from typing import List
+
+
+def weight_average(my_list: List[int]) -> int:
+    """Return the a tuple weight average."""
+
+    # Sum of total weight
+    weight_total: int = sum(weight for value, weight in my_list)
+    # Sum of weights times values
+    weight_sum: int = sum(weight * value for value, weight in my_list)
+    # Get average
+    result: float = weight_sum / weight_total
+
+    return result
