31e84c142a5759788fbf6aa253b1b999246a5e17
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 20:36:29 -0500

    multiply elements of a list by a given number

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""This program returns a list with all values
+multiplied by a number without using any loops"""
+from typing import List
+
+
+def multiply_list_map(my_list: List[int]=[], number: int=0) -> List[int]:
+    """Return. a new modified list."""
+    return list(map(lambda i: i * number, my_list))
