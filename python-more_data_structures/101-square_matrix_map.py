e1d4a7d01de68e811e2c4fb17b9b7e574546f217
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-30 10:31:38 -0500

    compute the square of each element in the matrix

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,1 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3computes the square value ofall integers of a matrix using map
+"""This program computes the square value
+of all integers of a matrix using map"""
+from typing import List
+
+
+def square_matrix_map(matrix: List[List[int]]) -> List[List[int]]:
+    """Returns the square of element in the matrix"""
+    return [[num * num for num in row] for row in matrix]
+            
