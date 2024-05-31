2315ce9eeb634c7ff7c4c64cc4de3a3f2c6963af
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-31 03:31:36 -0500

    print elements x number of times

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+"""This program prints x elements of a list."""
+from typing import List
+
+
+def safe_print_list(my_list: List[int]=[], x: int=0) -> int:
+    """Print elements x amount of time"""
+    elm_count: int = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            elm_count += 1
+        except IndexError:
+            break
+    print()
+    return elm_count
