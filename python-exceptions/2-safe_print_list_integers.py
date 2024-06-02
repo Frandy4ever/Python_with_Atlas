83ffa48b7055564089c315c05b9f42af5c10c90f
Frandy Slueue <frandy4ever@gmail.com>
2024-06-02 00:28:53 -0500

    print x elements of list and only integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""This program prints the first x elements of a list and only integers."""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Return the count of integer in the my_list"""
+    counter = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end=' ')
+            counter += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
+    return counter   
