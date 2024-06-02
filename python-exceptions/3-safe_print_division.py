769a1122b5444c978d66428abec3884f82d1eaa4
Frandy Slueue <frandy4ever@gmail.com>
2024-06-02 01:11:50 -0500

    handle zero division error

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""This program divides 2 integers and prints the result."""
+
+
+def safe_print_division(a, b):
+    """Handle Zero division error"""
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
