c7978adcf3126fc817d638aa5ace015c2dec056d
Frandy Slueue <frandy4ever@gmail.com>
2024-06-01 22:43:07 -0500

    check if value is integer or not

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This program prints an integer with "{:d}".format()."""
+from typing import Union
+
+
+def safe_print_integer(value: Union[int, str]) -> bool:
+    """Return boolean value if value is integer or not."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
