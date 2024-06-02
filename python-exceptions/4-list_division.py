591d89a4712a59d06b9d759f1c20cb89224ea82b
Frandy Slueue <frandy4ever@gmail.com>
2024-06-02 02:13:30 -0500

    calc to division between two list elements

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""This program divides element by element 2 lists
+while handling all errors."""
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Returns the result of lists divisions"""
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
+        except TypeError:
+            result = 0
+            print("wrong type")
+        except IndexError:
+            result = 0
+            print("out of range")
+        finally:
+            new_list.append(result)
+    
+    return new_list
